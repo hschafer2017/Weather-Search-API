6f41cbf7d7937a6646cb9b2c6de0d67cc75a1a4a
Haley Schafer <hschafer@umich.edu>
2018-05-02 16:02:36 +0000

    Weather Checker

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let request = new XMLHttpRequest();
 
 function displayNicely1(apiData){
 let dataFrom = JSON.parse(apiData);
-document.getElementById('weight').innerHTML = 
-"<strong>Weight: " + dataFrom.mass + "kg"; 
+document.getElementById('city-name').innerHTML = 
+"<strong>" + dataFrom.name + ", " +dataFrom.sys.country; 
 }
 
 function displayNicely2(apiData){
@@ -21,39 +21,46 @@ document.getElementById('gender').innerHTML =
 
 function displayNicely(apiData){
     let newData = JSON.parse(apiData);
-    let string1 = "<div><strong>Name: </strong> " + newData.name + "</div>"
-    string1 += "<div><strong>Weight: </strong>" + newData.mass + "</div>"
-    string1 += "<div><strong>Height: </strong>" + newData.height + "</div>"
-    string1 += "<div><strong>Gender: </strong>" + newData.gender + "</div>"
-    string1 += "<div><strong>Hair Color: </strong>" + newData.hair_color + "</div>"
+    console.log(newData);
+    let string1 = "<div><strong>Current Weather: </strong>" + newData.weather[0].main + "</div>"
+    string1 += "<div><strong>Temperature: </strong>" + newData.main.temp + "K </div>"
+    string1 += "<div><strong>Humidity: </strong>" + newData.main.humidity + "% </div>"
+    string1 += "<div><strong>Wind: </strong>" + newData.wind.speed + "kph </div>"
+    
+    // string1 += "<div><strong>Hair Color: </strong>" + newData.hair_color + "</div>"
     document.getElementById("data").innerHTML = 
     string1;
 }
   
-function displayNicely4(apiData) {
-    let newData = JSON.parse(apiData); 
-    let count = 1; 
-    for (let i in newData) {
-        count++;
-        document.getElementById("loops").innerHTML += "<br>" + i +" - " + newData[i] 
-        console.log(newData[i]);
-        if (count ==9) {
-            break;
-        }
-    }
-}    
+// function displayNicely4(apiData) {
+//     let newData = JSON.parse(apiData); 
+//     let count = 1; 
+//     for (let i in newData) {
+//         count++;
+//         document.getElementById("loops").innerHTML += "<br>" + i +" - " + newData[i] 
+//         console.log(newData[i]);
+//         if (count ==9) {
+//             break;
+//         }
+//     }
+// }    
 
 
 
 request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         displayNicely(this.responseText); 
-        // displayNicely1(this.responseText);
+        displayNicely1(this.responseText);
         // displayNicely2(this.responseText);
         // displayNicely3(this.responseText); 
-        displayNicely4(this.responseText); 
+        // displayNicely4(this.responseText); 
     }
 }
 
-request.open("GET", "https://swapi.co/api/people/1")
+function newCity() {
+    let city = 
+    return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=6ebea87dfc131fd5402906ce4b098ab8"
+}
+request.open("GET", newCity())
         request.send();
+        
